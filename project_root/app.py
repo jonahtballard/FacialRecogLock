0d51af9d989ecbf6300e151104761ab996557026
jonahtballard <143470778+jonahtballard@users.noreply.github.com>
2024-10-13 17:07:44 -0400

    a

--- project_root/app.py
+++ project_root/app.py
@@ -5,22 +5,24 @@ from facial_recognition.face_detection import FaceDetector  # Import the FaceDet
 
 app = Flask(__name__)
 camera = Camera()  # Initialize the camera
-face_detector = FaceDetector()  # Initialize the face detector
+
+#face_detector = FaceDetector()  # Initialize the face detector
 
 @app.route('/video_feed')
 def video_feed():
     return Response(camera.generate_frames(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
+'''
 @app.route('/toggle_face_detection')
 def toggle_face_detection():
     face_detector.toggle_detection()  # Toggle face detection on/off
     return 'Face detection toggled'
-
+'''
 @app.route('/')
 def index():
     return '<h1>Camera Stream</h1><img src="/video_feed">' 
+'''
 '<p><a href="/toggle_face_detection">Toggle Face Detection</a></p>'
-
+'''
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
