48155705739b7fdafc1770b6a2c5509c5c535e6a
jonahtballard <143470778+jonahtballard@users.noreply.github.com>
2024-11-07 17:19:33 -0500

    updates

--- project_root/app.py
+++ project_root/app.py
@@ -1,49 +1,41 @@
-from flask import Flask, Response, render_template
-import cv2
+from flask import Flask, Response, render_template_string, request
+import time
 
 app = Flask(__name__)
 
-# Initialize the camera
-camera = cv2.VideoCapture(0)
+frame = None   # global variable to keep single JPG, 
+               # at start you could assign bytes from empty JPG
 
-# Load the Haar Cascade for face detection
-face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+@app.route('/upload', methods=['PUT'])
+def upload():
+    global frame
+    
+    # keep jpg data in global variable
+    frame = request.data
+    
+    return "OK"
 
-def generate_frames():
+def gen():
     while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        
-        # Convert the frame to grayscale for face detection
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        
-        # Detect faces
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-
-        # Draw rectangles around faces
-        if len(faces) > 0:
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Green box for recognized faces
-        else:
-            cv2.putText(frame, "No face detected", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)  # Red text if no face
-
-        # Encode the frame in JPEG format
-        ret, buffer = cv2.imencode('.jpg', frame)
-        frame = buffer.tobytes()  # Convert to bytes
-        # Yield the frame in the required format for streaming
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+               b'Content-Type: image/jpeg\r\n'
+               b'\r\n' + frame + b'\r\n')
+        time.sleep(0.04) # my Firefox needs some time to display image / Chrome displays image without it 
+                         # 0.04s = 40ms = 25 frames per second 
+
+        
+@app.route('/video')
+def video():
+    if frame:
+        # if you use `boundary=other_name` then you have to yield `b--other_name\r\n`
+        return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    else:
+        return ""
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return 'image:<br><img src="/video">'
+    #return render_template_string('image:<br><img src="{{ url_for("video") }}">')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
+if __name__ == "__main__":
+    app.run(debug=True)#, use_reloader=False)
