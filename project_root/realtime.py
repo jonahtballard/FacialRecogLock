1845b3ab2339119ec7c06dc6803fad214151a041
mgkosten <madeline.kosten@uvm.edu>
2024-11-20 12:19:57 -0500

    Add lock features to work

--- project_root/realtime.py
+++ project_root/realtime.py
@@ -3,12 +3,19 @@ import cv2
 import sqlite3
 import numpy as np
 import os
+import RPi.GPIO as GPIO
+ 
 
+RELAY_PIN = 12 # GPIO pin controlled the solenoid lock via the relay module
 
 project_root = '../project_root/'
 
 db_path = os.path.join(project_root, 'facial_encodings.db')
 
+# GPIO Setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(RELAY_PIN, GPIO.OUT)
+GPIO.output(RELAY_PIN, GPIO.LOW)  # Lock the door initially
 
 # Load encodings from the database
 def load_encodings_from_db():
@@ -33,6 +40,7 @@ known_face_encodings, known_face_names = load_encodings_from_db()
 # Video capture and face recognition
 def generate_frames(known_face_encodings, known_face_names):
     video_capture = cv2.VideoCapture(0)
+    unlocked = False
 
     while True:
         ret, frame = video_capture.read()
@@ -43,6 +51,7 @@ def generate_frames(known_face_encodings, known_face_names):
         face_encodings = face_recognition.face_encodings(frame, face_locations)
 
         face_names = []
+        recognized = False
         for face_encoding in face_encodings:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
             name = "Unknown"
@@ -50,6 +59,7 @@ def generate_frames(known_face_encodings, known_face_names):
 
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
+                recognized = True
             face_names.append(name)
 
         for (top, right, bottom, left), name in zip(face_locations, face_names):
@@ -57,9 +67,19 @@ def generate_frames(known_face_encodings, known_face_names):
             cv2.rectangle(frame, (left, top), (right, bottom), color, 2)
             cv2.putText(frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 1, (255, 255, 255), 1)
 
+        if recognized and not unlocked:
+            print("Recognized face detected. Unlocking door...")
+            GPIO.output(RELAY_PIN, GPIO.HIGH)  # Unlock the door 
+            unlocked = True
+        elif not recognized and unlocked:     
+            print("No recognized face. Locking door...")
+            GPIO.output(RELAY_PIN, GPIO.LOW)  # Lock the door
+            unlocked = False
+
         ret, buffer = cv2.imencode('.jpg', frame)
         frame = buffer.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
     video_capture.release()
+    GPIO.cleanup()
